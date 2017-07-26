3b0ade6bca4378bff15004ff65d8fb13ffa626a8
Ed Mandy <ed@7400circuits.com>
2017-07-25 18:43:09 -0600

    Changing polling to better ISR and adding better low-batt shutdown

--- shutdown_daemon.py
+++ shutdown_daemon.py
@@ -33,10 +33,19 @@ import os
 
 # ==== constants
 __author__ = 'original from Paul W. Versteeg, hacked by Ed'
-VERSION = "3.1-"
+VERSION = "3.2-Freeplay"
 
 # ==== GPIO setup
 STOP_REQ = 20 # Stop/start button input, active low to shutdown RPi
+LOW_BATT = 7  # GPIO7 = low batt from MCU supervisor chip
+
+pwr_btn_timeout = 2.0   # how many seconds does the power button need to be held
+pwr_btn_edge_start = 0
+
+low_batt_timeout = 60.0
+low_batt_edge_start = 0
+
+
 
 def init():
       '''
@@ -50,11 +59,33 @@ def init():
       # This port is connected to a button
       # It is used to shutdown the RPi without the automatic restart
       GPIO.setup(STOP_REQ, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+      GPIO.setup(LOW_BATT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
       # setup the event to detect the button press to powerdown the RPi
       GPIO.add_event_detect(STOP_REQ, GPIO.RISING, callback=stop_req, bouncetime=20)
+      GPIO.add_event_detect(LOW_BATT, GPIO.FALLING, callback=low_batt_req, bouncetime=20)
+      return
+
+def pwr_btn_falling(STOP_REQ):
+      global pwr_btn_edge_start
+      # remove the edge detection for the duration of this ISR
+      GPIO.remove_event_detect(STOP_REQ)
+      pwr_btn_edge_start=0
+      #print 'power button fell'
+      GPIO.add_event_detect(STOP_REQ, GPIO.RISING, callback=stop_req, bouncetime=20)
+      return
+
+def low_batt_rising(LOW_BATT):
+      global low_batt_edge_start
+      # remove the edge detection for the duration of this ISR
+      GPIO.remove_event_detect(LOW_BATT)
+      low_batt_edge_start=0
+      #print 'low batt detect rose'
+      GPIO.add_event_detect(LOW_BATT, GPIO.FALLING, callback=low_batt_req, bouncetime=20)
+      return
 
 def stop_req(STOP_REQ):
+      global pwr_btn_edge_start
       '''
       Interrupt service routine that gets called when the start/stop button
       is pressed while the RPi is running.
@@ -66,23 +97,35 @@ def stop_req(STOP_REQ):
       otherwise, it is deemed an invalid request.
 
       '''
-      edge_start = time() # create a timestamp reference
-      # print 'edge_start=',edge_start
-      while (time() - edge_start) <= 2.0 : # check within a 2 Sec window
-          if GPIO.input(STOP_REQ) == 0 : 
-              # False signal
-              # print 'false signal time() - edge_start=',(time() - edge_start)
-              return
-
-      # print 'time() - edge_start=',(time() - edge_start)
-      # print 'time()',time()
-
       # remove the edge detection for the duration of this ISR
       GPIO.remove_event_detect(STOP_REQ)
+      
+      GPIO.add_event_detect(STOP_REQ, GPIO.FALLING, callback=pwr_btn_falling, bouncetime=20)
+      pwr_btn_edge_start = time() # create a timestamp reference
+      #print 'stop_req: pwr_btn_edge_start=',pwr_btn_edge_start
+      return
 
-      subprocess.call(['sudo poweroff'], shell=True, \
-          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+def low_batt_req(LOW_BATT):
+      global low_batt_edge_start
+      '''
+      Interrupt service routine that gets called when the low battery indicator
+      goes low while the RPi is running.
+
+      Powerdown in this case means that the RPI will become powerless and the
+      systemd watchdog will reboot the RPi in 10 seconds after poweroff.
+
+      To avoid incorrect button presses, the button must be pressed for >1 seconds
+      otherwise, it is deemed an invalid request.
+
+      '''
+      # remove the edge detection for the duration of this ISR
+      GPIO.remove_event_detect(LOW_BATT)
+      
+      GPIO.add_event_detect(LOW_BATT, GPIO.RISING, callback=low_batt_rising, bouncetime=20)
+      low_batt_edge_start = time() # create a timestamp reference
+      #print 'low_batt_req: low_batt_edge_start=',low_batt_edge_start
+      return
 
 def main():
 
@@ -91,7 +134,30 @@ def main():
       while True :
             # wait for a button press to shutdown the RPi
             # sleep to be nice
-            sleep(60)
+            if pwr_btn_edge_start <= 0 and low_batt_edge_start <= 0 :
+                #print 'sleep ',pwr_btn_timeout
+                sleep(pwr_btn_timeout) #sleep for the smallest timeout period
+            else :
+                #print 'sleep 0.1'
+                sleep(0.1)
+      
+            #print 'main: pwr_btn_edge_start=',pwr_btn_edge_start
+            if pwr_btn_edge_start > 0 :
+                if (time() - pwr_btn_edge_start) > pwr_btn_timeout :
+                    #print 'SHUTTING DOWN from PWR BTN'
+                    GPIO.remove_event_detect(STOP_REQ)
+                    GPIO.remove_event_detect(LOW_BATT)
+                    subprocess.call(['sudo poweroff'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    return
+
+            #print 'main: low_batt_edge_start=',low_batt_edge_start
+            if low_batt_edge_start > 0 :
+                if (time() - low_batt_edge_start) > low_batt_timeout :
+                    #print 'SHUTTING DOWN from LOW BATT'
+                    GPIO.remove_event_detect(STOP_REQ)
+                    GPIO.remove_event_detect(LOW_BATT)
+                    subprocess.call(['sudo poweroff'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    return
 
 
 if __name__ == '__main__':
