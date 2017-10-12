0a8a9a38fe7888db0d12fd7684490f5804b3c422
Unknown <freeplay@gmail.com>
2017-10-12 16:38:13 -0600

    install.sh mods

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-pwd=$(pwd)
-if [ "$pwd" -ne "/home/pi/Freeplay/Freeplay-Support/" ] ; then
+
+if [ "$PWD" != "/home/pi/Freeplay/Freeplay-Support" ] ; then
   echo "This script should be run from /home/pi/Freeplay/Freeplay-Support/ !"
   exit
 fi
 
 cp GPIO\ Controller\ 1.cfg /opt/retropie/configs/all/retroarch-joypads
 cp es_input.cfg /opt/retropie/configs/all/emulationstation/
-sed -i -e '$i \python /home/pi/Freeplay/Freeplay-Support/shutdown_daemon.py &\n' rc.local
+
+if grep -q "python /home/pi/Freeplay/Freeplay-Support/shutdown_daemon.py &" /etc/rc.local; then
+    echo "/etc/rc.local already contains shutdown script"
+else
+    sudo sed -i -e '$i \python /home/pi/Freeplay/Freeplay-Support/shutdown_daemon.py &\n' /etc/rc.local
+fi
 sudo cp autostart.sh /opt/retropie/configs/all
 sudo cp freeplaycfg.txt /boot
 sudo cp fpstartup.py /boot
