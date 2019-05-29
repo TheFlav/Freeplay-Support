eafdf1bde420930ef8abe3724b2dee85895dfc64
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-05-29 15:46:41 -0600

    added print loop for download/install errors

--- Freeplay_Full_Setup.sh
+++ Freeplay_Full_Setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ADDONS=( TheFlav/Freeplay-Support mootikins/FreeplayILI9341 TheFlav/mkarcade_joystick_rpi TheFlav/rpi-fbcp TheFlav/setPCA9633 )
-OPTIONS=$( printf '%s\n' "${ADDONS[@]}" | awk '{print v++, $1, "on" }')
+ADDONS=( TheFlav/FreeplaySupport mootikins/FreeplayILI9341 TheFlav/mk_arcade_joystickrpi TheFlav/rpi-fbcp TheFlav/setPCA9633 )
+OPTIONS=$( printf '%s\n' "${ADDONS[@]}" | awk '{print $1, v++, "on" }')
 
 cmd=(dialog --title "Install Addons" \
 	--separate-output \
@@ -13,15 +13,17 @@ CHOICES=$("${cmd[@]}" ${OPTIONS} 2>&1 >/dev/tty)
 clear
 
 ####################
-#   TODO REMOVE    #
+#   TODO Remove    #
 ####################
 rm -rf Freeplay
 
 mkdir Freeplay
 
-printf "Downloading selected Addons. If there are any prompts, press Enter. Check \u001b[32m~/Freeplay_Install_Log.txt if you encounter issues.\u001b[0m\n"
+printf "Downloading selected Addons. If there are any prompts, press Enter."
 
 pushd /home/pi/Freeplay/Freeplay-Support/Freeplay/ &> /dev/null
+
+DL_ERR=()
 
 for ADDON in $CHOICES
 do
@@ -29,8 +31,28 @@ do
 	if git clone https://github.com/${ADDONS["$ADDON"]} ; then
 		printf "\u001b[32mModule ${ADDONS["$ADDON"]} downloaded successfully\u001b[0m\n"
 	else
-		printf "\e[5;31;40mModule ${ADDONS["$ADDON"]} was NOT downloaded successfully\u001b[0m\n"
+		printf "\e[0;31;40mModule ${ADDONS["$ADDON"]} was NOT downloaded successfully\u001b[0m\n"
+		DL_ERR+=( "$ADDON" )
 	fi
 done
 
+INST_ERR=()
+
+for DIR in $(ls -d */)
+do
+	pushd $DIR &> /dev/null
+	printf "\u001b[36;1mInstalling $DIR...\u001b[0m\n"
+####################
+# TODO Add Install #
+####################
+	popd &> /dev/null
+done
 popd &> /dev/null
+
+printf "The following modules could not be downloaded:\n"
+for MODULE in ${DL_ERR[@]}
+do
+	printf "\t\e[0;31;40m${ADDONS["$MODULE"]}\u001b[0m\n"
+done
+
+
