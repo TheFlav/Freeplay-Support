57927c2863398c07ac0a8945bd9c44eebbdbea63
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-05-30 15:05:15 -0600

    easier to put more addons in later, have to use underscores in descriptions

--- Full_Setup.sh
+++ Full_Setup.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-ADDONS=( TheFlav/FreeplaySupport mootikins/FreeplayILI9341 TheFlav/mk_arcade_joystickrpi TheFlav/rpi-fbcp TheFlav/setPCA9633 )
-OPTIONS=$( printf '%s\n' "${ADDONS[@]}" | awk '{print $1, v++, "on" }')
+#Modules can be added here, separated by spaces
+#The description must use underscores instead of spaces
+ADDONS=(
+"TheFlav/Freeplay-Support General_Freeplay_Tools on"
+"TheFlav/rpi-fbcp Original_display_driver_(Zero) on"
+"mootikins/FreeplayILI9341 Precompiled_advanced_display_driver_(Non-Zero) on"
+"TheFlav/mk_arcade_joystick_rpi GPIO_joystick_driver_with_related_tools on"
+"TheFlav/setPCA9633 PWM_Brightness_controller_(requires_add-on) on"
+)
+
+echo "${ADDONS[@]}"
 
 cmd=(dialog --title "Install Addons" \
 	--separate-output \
 	--ok-label "Install" \
 	--checklist "Select options:" 0 0 0)
 
-CHOICES=$("${cmd[@]}" ${OPTIONS} 2>&1 >/dev/tty)
+CHOICES=$("${cmd[@]}" ${ADDONS[@]} 2>&1 >/dev/tty)
 
+echo "${CHOICES[@]}"
 clear
 
 ####################
@@ -24,24 +34,33 @@ printf "Downloading selected Addons. If there are any prompts, press Enter."
 pushd /home/pi/Freeplay/Freeplay-Support/Freeplay/ &> /dev/null
 
 DL_ERR=()
-
 for ADDON in $CHOICES
 do
-	printf "\nDownloading module ${ADDONS["$ADDON"]}...\u001b[0m\n"
-	if git clone https://github.com/${ADDONS["$ADDON"]} ; then
-		printf "\u001b[32mModule ${ADDONS["$ADDON"]} downloaded successfully\u001b[0m\n"
+	printf "\nDownloading module "$ADDON"...\u001b[0m\n"
+	if git clone https://github.com/"$ADDON" ; then
+		printf "\u001b[32mModule "$ADDON" downloaded successfully\u001b[0m\n"
 	else
-		printf "\e[0;31;40mModule ${ADDONS["$ADDON"]} was NOT downloaded successfully\u001b[0m\n"
+		printf "\e[0;31;40mModule "$ADDON" was NOT downloaded successfully\u001b[0m\n"
 		DL_ERR+=( "$ADDON" )
 	fi
 done
 
-INST_ERR=()
+if [ ${#DL_ERR[@]} -eq 0 ]; then
+	printf "\n\u001b[32mAll selected modules downloaded successfully\u001b[0m\n"
+else
+	printf "\n\e[0;31;40mThe following modules could not be downloaded:\u001b[0m\n"
+	for MODULE in ${DL_ERR[@]}
+	do
+		printf "\t\e[0;31;40m"$MODULE"\u001b[0m\n"
+	done
+fi
 
+INST_ERR=()
+printf "\n\u001b[36;1mInstalling downloaded modules\u001b[0m\n"
 for DIR in $(ls -d */)
 do
 	pushd $DIR &> /dev/null
-	printf "\u001b[36;1mInstalling $DIR...\u001b[0m\n"
+	printf "\t\u001b[36;1m$DIR...\u001b[0m\n"
 ####################
 # TODO Add Install #
 ####################
@@ -49,10 +68,12 @@ do
 done
 popd &> /dev/null
 
-printf "The following modules could not be downloaded:\n"
-for MODULE in ${DL_ERR[@]}
-do
-	printf "\t\e[0;31;40m${ADDONS["$MODULE"]}\u001b[0m\n"
-done
-
-
+if [ ${#INST_ERR[@]} -eq 0 ]; then
+	printf "\n\u001b[32mAll downloaded modules installed successfully\u001b[0m\n"
+else
+	printf "\n\e[0;31;40mThe following modules could not be installed:\u001b[0m\n"
+	for MODULE in ${INST_ERR[@]}
+	do
+		printf "\t\e[0;31;40m"$MODULE"\u001b[0m\n"
+	done
+fi
